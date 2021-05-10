d1d058dda68fa59dfa719368379d9002d19b4627
Denisa-Manta <denisavalentina10@yahoo.com>
2021-05-10 16:30:42 +0300

    BEAUT-7 Client Alegere Optiune-Adaugare buton inapoi

--- src/main/java/org/BSA/controllers/PaginaClientAlegereOptiune.java
+++ src/main/java/org/BSA/controllers/PaginaClientAlegereOptiune.java
@@ -1,15 +1,21 @@
 package org.BSA.controllers;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.BSA.model.User;
 import org.BSA.services.UserService;
 import java.awt.*;
 import static org.BSA.controllers.Login.getUserCurrent;
 
 public class PaginaClientAlegereOptiune {
 
-
+    @FXML
+    private Button ButonInapoi;
     @FXML
     private Button Tuns;
     @FXML
@@ -28,6 +34,11 @@ public class PaginaClientAlegereOptiune {
     private Text Mesaj;
     @FXML
     private TextField AlegereOptiuneClient;
+    @FXML
+    private User user;
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public void adaugareTuns() {
         UserService.adaugareOptiune(getUserCurrent,"Tuns");
@@ -56,5 +67,19 @@ public class PaginaClientAlegereOptiune {
     public void adaugareTratamenteFaciale() {
         UserService.adaugareOptiune(getUserCurrent,"Tratamentefaciale");
         Mesaj.setText("Optiunea de tratamente faciale a fost selectata.");
+    }
+    @FXML
+    public void handleInapoiAction() throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("paginaClient.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) (ButonInapoi.getScene().getWindow());
+            stage.setScene(new Scene(root));
+            PaginaClient client = loader.getController();
+            client.setUser(user);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Eroare");
+        }
     }
 }
