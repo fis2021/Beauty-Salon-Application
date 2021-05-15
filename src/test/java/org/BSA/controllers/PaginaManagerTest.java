f3b776424bc06ce10a4aec41f64b3cde34145028
unknown <iulia.mircioaga@student.upt.ro>
2021-05-15 15:45:21 +0300

    testare - Pagina Manager

---
+++ src/test/java/org/BSA/controllers/PaginaManagerTest.java
@@ -0,0 +1,65 @@
+package org.BSA.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.BSA.exceptions.UsernameAlreadyExistsException;
+import org.BSA.exceptions.UsernameDoesNotExists;
+import org.BSA.services.FileSystemService;
+import org.BSA.services.UserService;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import java.io.IOException;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testfx.assertions.api.Assertions.assertThat;
+@ExtendWith(ApplicationExtension.class)
+class PaginaManagerTest {
+    @AfterEach
+    void tearDown() {
+
+        UserService.database.close();
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        FileSystemService.APPLICATION_FOLDER = ".test-registration-example";
+        FileSystemService.initDirectory();
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        UserService.initDatabase();
+
+    }
+    @Start
+    void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("paginaManager.fxml"));
+        primaryStage.setTitle("BEAUTY SALON");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+    @Test
+    void testPaginaManager(FxRobot robot) {
+        robot.clickOn("#VizualizareListaClientiInAsteptare");
+        robot.clickOn("#inapoi1");
+        robot.clickOn("#AcceptareRespingereProgramare");
+        robot.clickOn("#inapoi2");
+        robot.clickOn("#AdaugareServiciiSiPreturi");
+        robot.clickOn("#inapoi3");
+        robot.clickOn("#deconectare");
+
+
+    }
+
+
+}
